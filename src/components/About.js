0d9567d82a6464e831a8b7d1c28a78a9c23dc06f
Ipaye Rpdoyah <Ipayedamilolaao@gmail.com>
2024-08-21 11:12:07 -0700

    new

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <section id="about">
-      <h2>Hello, my name is Rodiyah Ipaye.</h2>
+      <h2>Hello, my name<br />
+        is Rodiyah Ipaye.</h2>
       <p>
-        I specialiez in creating the user interface (UI) and
-        user experience (UX) of a website or web application. I am  responsible
-        for translating design mockups into functional, interactive, and
-        visually appealing web pages. This involves working with languages
-        like HTML, CSS, and JavaScript, as well as frameworks and libraries
+        I specialiez in creating the user interface (UI) and<br />
+        user experience (UX) of a website or web application. I am  responsible<br />
+        for translating design mockups into functional, interactive, and<br />
+        visually appealing web pages. This involves working with languages<br />
+        like HTML, CSS, and JavaScript, as well as frameworks and libraries<br />
         such as React, Angular, or Vue.js.</p>
     </section>
   );

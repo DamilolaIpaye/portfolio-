8102d9ecd4d97a879865849d7c3e34b666de37e3
Ipaye Rpdoyah <Ipayedamilolaao@gmail.com>
2024-08-30 08:29:57 -0700

    new

--- src/components/About.js
+++ src/components/About.js
@@ -13,21 +13,21 @@ const About = () => {
   };
 
   return (
-    <section id="about" className='about'>
-      <img src='/image/me.png' className='topRightImage' />
-      <img src='/image/yellow-bg.png' className='bg' />
+    <section id="about" className="about">
+      <img src="/image/me.png" className="topRightImage" alt="me" />
+      <img src="/image/yellow-bg.png" className="bg" alt="Background" />
 
-      <h1>Hello, my name<br />
-        is Rodiyah Ipaye.</h1>
+      <h1>Hello, my name<br /> is Rodiyah Ipaye.</h1>
       <p>
         I specialize in creating the user interface (UI) and<br />
         user experience (UX) of a website or web application. I am responsible<br />
         for translating design mockups into functional, interactive, and<br />
         visually appealing web pages. This involves working with languages<br />
         like HTML, CSS, and JavaScript, as well as frameworks and libraries<br />
-        such as React, Angular, or Vue.js.</p>
-      <button className='linked' onClick={handleLinkedInClick}>LinkedIn</button>
-      <button className='project-button' onClick={handleProjectButtonClick}>Projects</button>
+        such as React, Angular, or Vue.js.
+      </p>
+      <button className="linked" onClick={handleLinkedInClick}>LinkedIn</button>
+      <button className="project-button" onClick={handleProjectButtonClick}>Projects</button>
     </section>
   );
 };

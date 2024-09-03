13b8bee1e340eca2523f3447b0365d8787ea0638
Ipaye Rpdoyah <Ipayedamilolaao@gmail.com>
2024-09-03 04:28:50 -0700

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                     <img src='/image/insta.png' alt='Instagram' />
                 </a>
                 <a href='https://www.linkedin.com/in/damilola-ipaye-8413b1243/' target='_blank' rel='noopener noreferrer'>
-                    <img src='/image/in.png' alt='LinkedIn' />
+                    <img src='/image/linkedin.png' alt='LinkedIn' />
                 </a>
                 <img src='/image/mail.png' alt='Email' />
                 <h6>Rodiyah 2024</h6>

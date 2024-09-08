de923c995df0f6cb69d1aa564d8662fa73261d7f
Ipaye Rpdoyah <Ipayedamilolaao@gmail.com>
2024-09-08 14:07:40 -0700

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,13 @@ const Footer = () => {
         }}>
             <div className='footer'>
                 <a href='https://www.instagram.com/ipaye_rodiah/' target='_blank' rel='noopener noreferrer'>
-                    <img src='/image/insta.png' alt='Instagram' />
+                    <img src='/image/insta.png' alt='Instagram' className='footer-icon' />
                 </a>
                 <a href='https://www.linkedin.com/in/damilola-ipaye-8413b1243/' target='_blank' rel='noopener noreferrer'>
-                    <img src='/image/linkedin.png' alt='LinkedIn' />
+                    <img src='/image/linkedin.png' alt='LinkedIn' className='footer-icon' />
                 </a>
                 <a href='mailto:ipayedamilolaao@gmail.com/' target='_blank' rel='noopener noreferrer'>
-                    <img src='/image/mail.png' alt='Email' />
+                    <img src='/image/mail.png' alt='Email' className='footer-icon' />
                 </a>
                 <h6>Rodiyah 2024</h6>
             </div>

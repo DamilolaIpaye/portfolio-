282b8f6b87a898a60089372b32f0228d32d7ebb5
Ipaye Rpdoyah <Ipayedamilolaao@gmail.com>
2024-09-08 14:10:25 -0700

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,15 @@ const Navbar = () => {
             <h3 className='name-sm'>Rodiyah</h3>
             <h3 className='name-md'>Rodiyah Ipaye</h3>
             <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
             </ul>
         </nav>
     );
